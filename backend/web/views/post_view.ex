705d5b10e1dedd46f7833485df4b243527a36b10
James Hamet <jameshamet@gmail.com>
2016-02-19 13:04:43 -0500

    FIX backend datum changed to posts

--- backend/web/views/post_view.ex
+++ backend/web/views/post_view.ex
@@ -2,11 +2,11 @@ defmodule Backend.PostView do
   use Backend.Web, :view
 
   def render("index.json", %{posts: posts}) do
-    %{data: render_many(posts, Backend.PostView, "post.json")}
+    %{posts: render_many(posts, Backend.PostView, "post.json")}
   end
 
   def render("show.json", %{post: post}) do
-    %{data: render_one(post, Backend.PostView, "post.json")}
+    %{post: render_one(post, Backend.PostView, "post.json")}
   end
 
   def render("post.json", %{post: post}) do
