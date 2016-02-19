2ad953cc7f23d120cc72e9527931b366a0cbb73d
James Hamet <jameshamet@gmail.com>
2016-02-19 12:02:14 -0500

    Installed corsica to backend

--- backend/mix.exs
+++ backend/mix.exs
@@ -35,6 +35,7 @@ defmodule Backend.Mixfile do
      {:phoenix_ecto, "~> 2.0"},
      {:phoenix_html, "~> 2.4"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
+     {:corsica, "~> 0.4"},
      {:gettext, "~> 0.9"},
      {:cowboy, "~> 1.0"}]
   end
