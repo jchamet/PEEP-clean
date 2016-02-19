c4be59552f84b237c0fae25bd1df2e7585ea0c86
James Hamet <jameshamet@gmail.com>
2016-02-19 11:46:55 -0500

    Added API endpoint to frontend

--- frontend/config/environment.js
+++ frontend/config/environment.js
@@ -26,6 +26,16 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    API_HOST: 'http://localhost:4000',
+    ENV.contentSecurityPolicy = {
+      'default-src': "'none'",
+      'script-src': "'self'",
+      'font-src': "'self'",
+      'connect-src': "'self' http://localhost:4000",
+      'img-src': "'self'",
+      'style-src': "'self'",
+      'media-src': "'self'"
+    };
   }
 
   if (environment === 'test') {
