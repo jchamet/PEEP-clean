19ad0c6f84a52a9d4f45b18d48bf9eea96ea0837
James Hamet <jameshamet@gmail.com>
2016-02-19 12:23:29 -0500

    Added serializer to frontend

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -1,5 +1,5 @@
 import DS from 'ember-data';
-import ENV from 'frontend/config/environment'
+import ENV from 'frontend/config/environment';
 
 export default DS.JSONAPIAdapter.extend({
   host: ENV.APP.API_HOST
