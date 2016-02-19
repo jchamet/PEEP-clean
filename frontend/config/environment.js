c840980f05641a4a082b666ac0b45f0e04fbf533
James Hamet <jameshamet@gmail.com>
2016-02-19 11:55:54 -0500

    FIX syntax in frontend config

--- frontend/config/environment.js
+++ frontend/config/environment.js
@@ -26,7 +26,7 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
-    API_HOST: 'http://localhost:4000',
+    ENV.APP.API_HOST = 'http://localhost:4000',
     ENV.contentSecurityPolicy = {
       'default-src': "'none'",
       'script-src': "'self'",
